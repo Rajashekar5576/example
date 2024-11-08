4f9e9d5920604cf5eeb7bd932318728562508202
Rajashekar <chinoorirajashekarreddy@gmail.com>
2024-11-08 19:09:55 +0530

    made some changes

---
+++ example.js
@@ -0,0 +1,40 @@
+
+function greet(name) {
+    let greeting = `Hello, ${name}!`;
+    return greeting;
+}
+
+var greet1 = greet("Rajesh");
+console.log(greet1);
+
+var greet2 = greet("Aditya");
+console.log(greet2);
+
+
+
+
+
+
+
+
+
+
+
+// console.log("start")
+
+// for(i=0; i<10; i++) {
+//     console.log(i)
+// }
+
+// console.log("end")
+
+
+
+
+
+
+
+
+
+
+
